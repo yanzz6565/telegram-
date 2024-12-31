1bcd34cab5bc66f2c10b49eb794c06d4cb820f2f
yanzz6565 <sdoni7630@gmail.com>
2024-12-31 08:45:29 +0700

    Add files via upload

---
+++ Xstuns3.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7660240073:AAEsfvsbCnADL3a_GQLCOTqPm33dSbkQORw", // Token bot Telegram
+  OWNER_ID: "6521470375", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
